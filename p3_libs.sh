32f25e426ad365bb26414aed393ffa2c9a8515ce
Daniel Abramov <34603514+RiskofStorm@users.noreply.github.com>
2018-07-01 14:10:47 +0300

    Add files via upload

--- p3_libs.sh
+++ p3_libs.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
-sudo -i
-apt-get install python-pip
-pip install --upgrade pip
-apt-get -y install ipython ipython-notebook
+
+sudo apt-get install python-pip
+sudo pip install --upgrade pip
+sudo pip3 install ipython[all]
 pip install jupyter
 pip install pandas
 pip install scikit-learn
