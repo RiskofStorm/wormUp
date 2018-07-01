1f92bc2c91e4dc2cacc893b922c6ca3c28053f11
Daniel Abramov <34603514+RiskofStorm@users.noreply.github.com>
2018-07-01 14:04:25 +0300

    Add files via upload

---
+++ p3_libs.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+sudo -i
+apt-get install python-pip
+pip install --upgrade pip
+apt-get -y install ipython ipython-notebook
+pip install jupyter
+pip install pandas
+pip install scikit-learn
+pip install scipy
+pip install psycopg2
+pip install pyspark
+pip install nltk
